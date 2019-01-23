e7f08038dac7cb98d1f6cbfe70041304d8c7bfc2
Fernando Hernáez <fernando.herlo@gmail.com>
2019-01-23 21:35:54 +0100

    Bugs

--- src/components/List.vue
+++ src/components/List.vue
@@ -170,13 +170,6 @@ export default {
       </span>
       <b-popover target="popoverAction" placement="bottomleft" container="popoverContent">
         <div class="action-wrap">
-          <a class="btn new" @click="__newItem()" v-if="canCreateNew">
-            <span v-html="config.buttons.newName" :title="config.buttons.newName" v-if="config.buttons.newName"></span>
-            <icon name="plus-circle" v-else></icon>
-          </a>
-          <template v-if="__checkComponentExists(config.coreExtendScopePl + '-btns')">
-            <div :is="config.coreExtendScopePl + '-btns'" ref="btnsdefault2"></div>
-          </template>
           <a class="btn print" @click="$helper.print()">
             <span v-html="config.buttons.printName" :title="config.buttons.printName" v-if="config.buttons.printName"></span>
             <icon name="print" v-else></icon>
