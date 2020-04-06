da72d5a4210350d180387b27a1fd8a9ae852fc5e
Fernando Hernáez <fernando.herlo@gmail.com>
2020-04-06 11:00:14 +0200

    Bugs

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -63,6 +63,7 @@ export default {
     // Degub
     EventBus.$log.debug('MUTATIONS')
     state.allByParent = state.all.filter( function(item) {
+      EventBus.$log.warning(item[storeRelated])
       return item[storeRelated].forEach( function(related) {
         return related.id === id_parent
       })
