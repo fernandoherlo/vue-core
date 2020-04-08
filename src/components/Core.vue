29414398bbf66e4a254f120ad4717cb4151ef195
Fernando Hernáez <fernando.herlo@gmail.com>
2020-04-08 11:57:16 +0200

    Bugs

--- src/components/Core.vue
+++ src/components/Core.vue
@@ -7,6 +7,13 @@
 */
 export default {
   methods: {
+  },
+  beforeRouteUpdate(to, from, next) {
+    // Degub
+    this.$log.debug(to)
+    this.$log.debug(from)
+    this.name = to.params.name
+    next()
   }
 }
 </script>
