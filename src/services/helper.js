9ed20fdc34d8740708c028b77842a17ecff1c3ce
Fernando Hernáez <fernando.herlo@gmail.com>
2020-04-10 20:17:54 +0200

    Bugs

--- src/services/helper.js
+++ src/services/helper.js
@@ -58,6 +58,18 @@ let methodsHelper = {
       return days + "d"
     }
   },
+  toHmsFromMs (duration) {
+    // var milliseconds = parseInt((duration % 1000) / 100)
+    var seconds = Math.floor((duration / 1000) % 60)
+    var minutes = Math.floor((duration / (1000 * 60)) % 60)
+    var hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+    hours = (hours < 10) ? "0" + hours : hours
+    minutes = (minutes < 10) ? "0" + minutes : minutes
+    seconds = (seconds < 10) ? "0" + seconds : seconds
+
+    return hours + 'h ' + minutes + '\' ' + seconds + '"'
+  },
   // Parse ID
   getID (ID) {
     // Is number
